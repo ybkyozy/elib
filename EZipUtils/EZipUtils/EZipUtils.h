b930ffe47c012e9c128524ab49e75b72f3d1fc0d
kyozy <ybkyozy@163.com>
2018-08-08 13:31:08 +0800

    初步定义

---
+++ EZipUtils/EZipUtils/EZipUtils.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <WTypes.h>
+
+#ifndef __E_STATIC_LIB
+
+#define LIB_GUID_STR "FB41C2BE2B4349F1B187D8CE15D3F292" /*GUID串: // {FB41C2BE-2B43-49F1-B187-D8CE15D3F292}*/
+#define LIB_MajorVersion 1 /*库主版本号*/
+#define LIB_MinorVersion 0 /*库次版本号*/
+#define LIB_BuildNumber 20170802 /*构建版本号*/
+#define LIB_SysMajorVer 3 /*系统主版本号*/
+#define LIB_SysMinorVer 0 /*系统次版本号*/
+#define LIB_KrnlLibMajorVer 3 /*核心库主版本号*/
+#define LIB_KrnlLibMinorVer 0 /*核心库次版本号*/
+#define LIB_NAME_STR "elibTest" /*支持库名*/
+#define LIB_DESCRIPTION_STR "测试用" /*功能描述*/
+#define LIB_Author "kyozy" /*作者名称*/
+#define LIB_ZipCode "644000" /*邮政编码*/
+#define LIB_Address "" /*通信地址*/
+#define LIB_Phone	"" /*电话号码*/
+#define LIB_Fax		"" /*QQ号码*/
+#define LIB_Email	 "" /*电子邮箱*/
+#define LIB_HomePage "" /*主页地址*/
+#define LIB_Other	"" /*其它信息*/
+#define LIB_TYPE_COUNT 1 /*命令分类数量*/
+#define LIB_TYPE_STR "0000基本命令\0""\0" /*命令分类*/
+#endif
