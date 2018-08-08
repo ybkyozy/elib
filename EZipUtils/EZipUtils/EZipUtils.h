e1f8f85d73b85806f2d539aa899c13ee892f1401
kyozy <ybkyozy@163.com>
2018-08-09 01:52:40 +0800

    2018.08.09

--- EZipUtils/EZipUtils/EZipUtils.h
+++ EZipUtils/EZipUtils/EZipUtils.h
@@ -12,16 +12,21 @@
 #define LIB_SysMinorVer 0 /*系统次版本号*/
 #define LIB_KrnlLibMajorVer 3 /*核心库主版本号*/
 #define LIB_KrnlLibMinorVer 0 /*核心库次版本号*/
-#define LIB_NAME_STR "elibTest" /*支持库名*/
-#define LIB_DESCRIPTION_STR "测试用" /*功能描述*/
+#define LIB_NAME_STR "EZipUtils" /*支持库名*/
+#define LIB_DESCRIPTION_STR "ZIP压缩解压操作支持库" /*功能描述*/
 #define LIB_Author "kyozy" /*作者名称*/
 #define LIB_ZipCode "644000" /*邮政编码*/
 #define LIB_Address "" /*通信地址*/
 #define LIB_Phone	"" /*电话号码*/
-#define LIB_Fax		"" /*QQ号码*/
+#define LIB_Fax		"75602718" /*QQ号码*/
 #define LIB_Email	 "" /*电子邮箱*/
 #define LIB_HomePage "" /*主页地址*/
 #define LIB_Other	"" /*其它信息*/
-#define LIB_TYPE_COUNT 1 /*命令分类数量*/
+#define LIB_TYPE_COUNT 0 /*命令分类数量*/
 #define LIB_TYPE_STR "0000基本命令\0""\0" /*命令分类*/
 #endif
+
+struct ZipStruct
+{
+	HZIP m_hZip;
+};
